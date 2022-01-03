37bae3ee0f157d913ddb514459722174a0537349
oHate <ohateign@gmail.com>
2022-01-02 22:03:50 -0600

    Fix incorrect import

--- src/main/java/cc/minetale/mlib/util/TeamUtil.java
+++ src/main/java/cc/minetale/mlib/util/TeamUtil.java
@@ -1,6 +1,6 @@
 package cc.minetale.mlib.util;
 
-import cc.minetale.commonlib.api.Rank;
+import cc.minetale.commonlib.grant.Rank;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
